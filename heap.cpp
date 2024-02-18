1b1f821048e128c00983f5093ddaa8a05dd9b9b7
AlphaNoobShit <122150255+AlphaNoobShit@users.noreply.github.com>
2024-02-18 10:14:38 +0000

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 void heapify(int array[], int size, int i)
 {
+
+    // for (int i = size / 2 - 1; i >= 0; i--)
+    //     heapify(array, size, i);
+
     int largest = i, left = 2 * i + 1, right = 2 * i + 2;
 
     if (left < size && array[left] > array[largest])
@@ -19,11 +23,32 @@ void heapify(int array[], int size, int i)
     }
 }
 
-void convert_to_heap(int array[], int size)
+// void convert_to_heap(int array[], int size)
+// {
+
+//     for (int i = size / 2 - 1; i >= 0; i--)
+//         heapify(array, size, i);
+// }
+
+void heap_sort(int array[], int size)
 {
 
+    // convert_to_heap(array, size);
     for (int i = size / 2 - 1; i >= 0; i--)
         heapify(array, size, i);
+
+    cout << "Heap Array: ";
+    for (int i = 0; i < size; i++)
+        cout << array[i] << " ";
+    cout << endl;
+    // int sortedArray[size];
+    for (int j = size - 1; j > 0; j--)
+    {
+        swap(array[j], array[0]);
+        heapify(array, j, 0);
+        // convert_to_heap(array, j);
+        // size --;
+    }
 }
 
 int main()
@@ -47,14 +72,12 @@ int main()
 
     cout << endl;
 
-    convert_to_heap(array, size);
+    heap_sort(array, size);
 
-    cout << "Heap Array: ";
+    cout << "Sorted Heap Array: ";
     for (int i = 0; i < size; i++)
         cout << array[i] << " ";
     cout << endl;
 
     return 0;
-    
 }
-
